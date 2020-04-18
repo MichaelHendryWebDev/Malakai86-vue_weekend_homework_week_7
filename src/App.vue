a15363dec292b122f51001104bf077d91bed76d2
mike <>
2020-04-18 12:09:03 +0100

    eventbus added and character list added to array

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <h1>hello</h1>
+    <h2>{{characters[0]}}</h2>
+  </div>
+</template>
+
+<script>
+import { eventBus } from './main.js'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      characters: [],
+      selectedCharacter: null
+    };
+  },
+  mounted(){
+    fetch('https://www.breakingbadapi.com/api/characters')
+    .then(res => res.json())
+    .then(characters => this.characters = characters)
+
+    eventBus.$on('character-selected', (character) => {
+      this.selectedCharacter = character;
+    })
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+div {
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
